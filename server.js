8b53588632871b696d2eb10564cf8e1abbee2937
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-12-02 18:12:21 -0300

    feat(server.js): adding nodemon package and completing module 1 of the course

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ import http from "http";
 
 const PORT = 3000;
 
+const rotas = {
+    "/": "Curso de Node.JS",
+    "/livros": "Rota de Livros",
+    "/editoras": "Rota de Editoras"
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Curso de Node.JS");
+    res.end(rotas[req.url]);
 });
 
 server.listen(PORT, () => {
     console.log("Servidor ouvindo!");
-    console.log("Na porta: http://localhost:3000");
 });
