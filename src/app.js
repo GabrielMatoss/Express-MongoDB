6b0e8592cb99f3a27cb492c45d217e42a53c3195
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-12-04 14:20:38 -0300

    feat(app.js): completion of module 2, finishing creation of the basic crud

--- src/app.js
+++ src/app.js
@@ -12,7 +12,13 @@ const books = [
         id: 2,
         title: "O Hobbit"
     }
-]
+];
+
+    function searchBook(id) {
+        return books.findIndex((book) => {
+            return book.id === Number(id);
+        });
+    }
 
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.JS");
@@ -22,9 +28,26 @@ app.get("/books", (req, res) => {
     res.status(200).json(books);
 });
 
+app.get("/books/:id", (req, res) => {
+    const index = searchBook(req.params.id);
+    res.status(200).json(books[index]);
+});
+
 app.post("/books", (req, res) => {
     books.push(req.body);
     res.status(201).send("Livro cadastrado com sucesso!");
-})
+});
+
+app.put("/books/:id", (req, res) => {
+    const index = searchBook(req.params.id);
+    books[index].title = req.body.title;
+    res.status(200).json(books) 
+});
+
+app.delete("/books/:id", (req, res) => {
+    const index = searchBook(req.params.id);
+    books.splice(index, 1);
+    res.status(200).send("Livro removido com sucesso!");
+});
 
 export default app;
