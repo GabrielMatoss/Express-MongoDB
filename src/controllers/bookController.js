4742e9dfd1a165a862cb303f62ed08d86d90fadc
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-12-07 13:54:11 -0300

    feat(bookcontroller.js): adding method of registering books

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -7,11 +7,19 @@ class BookController {
             const bookList = await book.find({});
             res.status(200).json(bookList);            
         } catch (error) {
-            res.status(500).json({ message: `${error.message} - falha na requisição` });
+            res.status(500).json({ message: `${error.message} - request failure` });
         }
-
     }; 
 
+    static async registerBook (req, res) {
+        try {
+            const newBook = await book.create(req.body);
+            console.log(newBook);
+            res.status(201).json({ message: "successfully created", book: newBook });
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - request failure` });
+        }
+    };
 };
 
 export default BookController;
