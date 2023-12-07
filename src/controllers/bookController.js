609f707e0066fd4293a3eb2a1a476ce402b84d71
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-12-07 10:42:40 -0300

    feat(bookcontroller.js): adding try/catch block

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -3,8 +3,13 @@ import book from "../models/Book.js"
 class BookController {
 
     static async listBooks (req, res) {
-        const bookList = await book.find({});
-        res.status(200).json(bookList);
+        try {
+            const bookList = await book.find({});
+            res.status(200).json(bookList);            
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - falha na requisição` });
+        }
+
     }; 
 
 };
