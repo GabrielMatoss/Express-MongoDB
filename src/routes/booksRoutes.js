f6e9d2018e25edc450ea0c0aa37ab54c68155892
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-12-08 09:11:40 -0300

    feat(booksroutes.js): adding new routes related to new methods

--- src/routes/booksRoutes.js
+++ src/routes/booksRoutes.js
@@ -4,5 +4,8 @@ import BookController from "../controllers/bookController.js";
 const routes = express.Router();
 
 routes.get("/books", BookController.listBooks);
+routes.get("/books/:id", BookController.listBooksById);
+routes.post("/books", BookController.registerBook);
+routes.put("/books/:id", BookController.updateBook);
 
 export default routes;
