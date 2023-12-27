f38140b7d912a4f043256b3f7f4961102787b7e9
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-12-27 09:31:44 -0300

    refactor(errorhandler.js): using polymorphism to handle paging error

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 import BaseError from "../errors/BaseError.js";
 import BadRequest from "../errors/BadRequest.js";
 import ValidationError from "../errors/ValidationError.js";
-import NotFound from "../errors/NotFound.js";
 
 // eslint-disable-next-line no-unused-vars
 function errorHandler(error, req, res, next){
@@ -10,7 +9,7 @@ function errorHandler(error, req, res, next){
 		new BadRequest().sendResponse(res);
 	} else if (error instanceof mongoose.Error.ValidationError) {
 		new ValidationError(error).sendResponse(res);
-	} else if (error instanceof NotFound) {
+	} else if (error instanceof BaseError) {
 		error.sendResponse(res);
 	} else {
 		new BaseError().sendResponse(res);
